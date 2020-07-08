0f8125bede2987122614d1fdde1a257c697db3cf
Ahmed Hamdy <ahmedhamdyboraey@gmail.com>
2020-07-08 13:55:32 +0200

    s

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,51 +1,48 @@
 <template>
   <v-app>
+    <v-img class="header_Logo" :src="require('@/assets/imgs/pic1.jpg')" contain></v-img>
 
     <app-header />
-  
+
     <v-content>
-     
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
 
-    <v-footer
-      :fixed="fixed"
-      app
-    >
+    <v-footer :fixed="fixed" app>
       <span>&copy; 2019</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-import AppHeader from '@/components/AppHeader.vue' 
+import AppHeader from "@/components/AppHeader.vue";
 export default {
-  components:{
+  components: {
     AppHeader
   },
-  data () {
+  data() {
     return {
       clipped: false,
       drawer: false,
       fixed: false,
       items: [
         {
-          title: 'Welcome',
-          to: '/'
+          title: "Welcome",
+          to: "/"
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          icon: "mdi-chart-bubble",
+          title: "Inspire",
+          to: "/inspire"
         }
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
-    }
+      title: "Vuetify.js"
+    };
   }
-}
+};
 </script>
